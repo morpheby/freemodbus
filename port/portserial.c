37ab525aa380d474856efe7511462f6b1a5bcb66
Ilya Mikhaltsou <morpheby@gmail.com>
2025-03-29 04:19:26 +0300

    Fix invalid pins

--- port/portserial.c
+++ port/portserial.c
@@ -61,7 +61,7 @@
 /* ----------------------- Static variables ---------------------------------*/
 
 typedef struct {
-    uint8_t pin;
+    uint16_t pin;
     GPIO_TypeDef *gpio;
     GPIOSpeed_TypeDef speed;
     GPIOMode_TypeDef mode;
